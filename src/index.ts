283e8bcaf5a7ef5f937f0dbe9e45a9f4d6f03cf0
MarcQuimis <marc.quimis@gmail.com>
2024-11-04 23:07:13 +0100

    commit ex

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello, World!");
+const person: {name: string; age: number; hobbies: string[]} = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+  };
+
+console.log(person)
+
+function printUser(): {name: string; age: number; hobbies: string[]} {
+    return{
+    name: 'Marc',
+        age: 30,
+        hobbies: ['Sports', 'Cooking']
+    }
+} 
+
+let rest = printUser()
+console.log(rest);
+
+type User = {
+    name: string;
+    age: number;
+    mail?: string;
+
+}
+
+let user: User = {
+    name: 'Max',
+    age: 30
+}
+
+console.log(user);
+
+/*=====================*/
+type userInfo = {
+    name: string;
+    age: number;
+    mail?: string;
+}
+
+type userDetails = {
+    password: string;
+    isAdmin: boolean;
+} 
+type user = userInfo & userDetails;
+
+const Marc: user = {
+    name: 'Marc',
+    age: 30,
+    password: '1234',
+    isAdmin: true
+}
+
+console.log(Marc);
+
+/*=====================*/
+
+const myInfo = {
+    name: "Marc",
+    age: 30,
+    isAdmin: true,
+    greetRegular: function (){
+        return `${this.name}  ${this.age} `
+    },
+}
+
+let [namev2, age] = myInfo.greetRegular()
+console.log(`my name is ${namev2} and my age is ${age}`);
